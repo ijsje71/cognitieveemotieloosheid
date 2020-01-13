f600cc41031c478db207a00500daf31815c90b64
Diadrius <59838858+Diadrius@users.noreply.github.com>
2020-01-13 18:14:18 +0100

    new page

--- CE/src/app/app-routing.module.ts
+++ CE/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'mindfulness',
+    loadChildren: () => import('./mindfulness/mindfulness.module').then( m => m.MindfulnessPageModule)
   }
 ];
 
